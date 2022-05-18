97578f04cb37fd12807f31048577cbb1b4f9dd10
Angela Busheska <64707998+Angelaangie-ai@users.noreply.github.com>
2022-05-18 14:29:41 -0400

    Update part3_linkedlist.java

--- Assignment-1/part3_linkedlist.java
+++ Assignment-1/part3_linkedlist.java
@@ -1,22 +1,49 @@
-//Adds the node at the end of the list
-public void push(<Node> node)
-{
-  int data;
-  Node node = new Node(data);
- 
-    //If the linked list is the last
-    if (head == null)
-    {
-        head = new Node(data);
-        return;
-    }
-   node.next = null;
- 
-  //If the Linked List is not the last
-    Node node1 = head;
-    while (node1.next != null)
-        node1 = node1.next;
+	/* Appends a new node at the end. */
+	void push(Node node)
+	{
+		//If the head of the last node is empty
+		if (head == null)
+		{
+			//in that situation we will have another node at the end
+			head = node;
+			node.next = null; //The next node will be empty
+			return;
+		}
+		else {
 
-    node1.next = node;
-    return;
-}
+		//We pass through the nodes, checking for every single node
+		Node last = head;
+		while (last.next != null)
+			last = last.next;
+
+		//assigning a mode on the last one
+		last.next = node;
+		return;
+		}
+	}
+	
+
+    // Function to remove the last node //
+    static Node removeLastNode(Node head)
+    {
+    	//Start from the head node, if it is not empty, it should be empty
+        if (head == null)
+            return null;
+        
+        if (head.next == null) {
+            return null;
+        }
+ 
+        //Now that we made for the last node to be null
+        //we position the secondLastNode as the head
+        Node secondLastNode = head;
+        //Now we go through the array
+        while (secondlastNode.next != null)
+            secondLast = secondLast.next;
+ 
+        // Change next of second last
+        secondLast.next = null;
+ 
+        return head;
+    }
+ 
