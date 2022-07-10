5564f9deabd681e4ea6415aa047c4f03b9d57a8b
Angela Busheska <64707998+Angelaangie-ai@users.noreply.github.com>
2022-07-09 23:07:44 -0400

    Update bfs-graph.java

--- Assignment-2/bfs-graph.java
+++ Assignment-2/bfs-graph.java
@@ -2,6 +2,11 @@ public List<Integer> depthFirtst(int key){
  List<Boolean, Integer> visited = new HashMap<>();
  List<Integer> queue = new LinkedList<>();
  List<Integer> levels = new LinkedList<>();
-queue.add(key);
+ queue.addFirst(key);
+ while (queue.size() != 0) {
+   int node = queue.removeFirst();
+   queue.add(node);
+   levels.put(node.true);
   
+ }
 }
