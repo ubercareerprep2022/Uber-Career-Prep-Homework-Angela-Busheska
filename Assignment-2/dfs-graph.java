2ac5a9031cc1b70b361f1241376a8c8578722d17
Angela Busheska <64707998+Angelaangie-ai@users.noreply.github.com>
2022-07-09 23:21:23 -0400

    Update dfs-graph.java

--- Assignment-2/dfs-graph.java
+++ Assignment-2/dfs-graph.java
@@ -1,12 +1,16 @@
  List<Boolean, Integer> visited = new HashMap<>();
   List<Integer> queue = new LinkedList<>();
   List<Integer> levels = new LinkedList<>();
-  queue.add(key);
   queue.addLast(key);
   while (queue.size() != 0) {
     int node = queue.removeFirst();
     queue.add(node);
     levels.put(node.true);
-
+  for (int i; i<queue.size; i++){
+    while (levels.get(node) != null){
+      queue.addFirst(node)
+    }
+   }
   }
+  return levels;
  }
