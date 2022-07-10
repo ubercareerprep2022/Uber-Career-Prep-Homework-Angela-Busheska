a7c7de2e667e368cbfce2b3098fca8ca47897bc6
Angela Busheska <64707998+Angelaangie-ai@users.noreply.github.com>
2022-07-09 22:50:53 -0400

    Create bfs-graph.java

---
+++ Assignment-2/bfs-graph.java
@@ -0,0 +1,7 @@
+public List<Integer> depthFirtst(int key){
+ List<Boolean, Integer> visited = new HashMap<>();
+ List<Integer> queue = new LinkedList<>();
+ List<Integer> levels = new LinkedList<>();
+queue.add(key);
+  
+}
