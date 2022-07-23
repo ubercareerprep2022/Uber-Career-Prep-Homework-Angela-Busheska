efebdb2ad4e990a23fc784584f0e3a97da8c6675
Angela Busheska <64707998+Angelaangie-ai@users.noreply.github.com>
2022-07-23 19:50:15 -0400

    Update and rename Assignment-2/bfs-graph.java to Assignment-2/Graphs/exercise2.java

--- Assignment-2/Graphs/exercise2.java
+++ Assignment-2/Graphs/exercise2.java
@@ -1,16 +1,21 @@
 public List<Integer> depthFirtst(int key){
+ 
  List<Boolean, Integer> visited = new HashMap<>();
  List<Integer> queue = new LinkedList<>();
  List<Integer> levels = new LinkedList<>();
+ 
  queue.addFirst(key);
  while (queue.size() != 0) {
+  
    int node = queue.removeFirst();
    queue.addFirst(node);
    visited.put(node.true);
+  
    for (int i = 0; i<queue.size(); i++) {
     if (visited.get(node) != null) {
      levels.addFirst(node.val);
     }
+    
    }
  }
  return levels;
